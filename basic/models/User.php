ce2c203afc2dba13f006f819f2dc176894f77391
Магомедова Аминат Магомедовна <10190208@college.local>
2023-01-26 13:11:00 +0300

    '26.01.23'

---
+++ basic/models/User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $fio
+ * @property string $login
+ * @property string $email
+ * @property string $password
+ * @property int|null $admin
+ *
+ * @property Problem[] $problems
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['fio', 'login', 'email', 'password'], 'required'],
+            [['admin'], 'integer'],
+            [['fio', 'login', 'email', 'password'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'fio' => 'Fio',
+            'login' => 'Login',
+            'email' => 'Email',
+            'password' => 'Password',
+            'admin' => 'Admin',
+        ];
+    }
+
+    /**
+     * Gets query for [[Problems]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProblems()
+    {
+        return $this->hasMany(Problem::class, ['idUser' => 'id']);
+    }
+}
